1f4c1cc5138191487fb7cbf7e5b988680a6af085
HunterMahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-04-06 10:33:14 +0600

    Create Linear Algebra Foundations #2 - Matrix Subtraction.cpp

---
+++ Linear Algebra Foundations #2 - Matrix Subtraction.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    vector <int> ar={-3,-3,-3,-5,-5,-5,-3,-4,1};
+    for(int i: ar){
+        cout<<i<<endl;
+    }
+    return 0;
+}
