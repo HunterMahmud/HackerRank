c11bf30df54dc98a900f375c2ee95174e2a0b368
HunterMahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-04-06 10:13:17 +0600

    Create Linear Algebra Foundations #1 - Matrix Addition.cpp

---
+++ Linear Algebra Foundations #1 - Matrix Addition.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    vector<int> ar={5,7,9,9,11,13,5,6,8};
+    for(int i: ar){
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
