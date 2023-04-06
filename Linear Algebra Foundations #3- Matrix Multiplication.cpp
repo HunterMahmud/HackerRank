3629dc98de71949d9c7c336a6b096e0241efb178
HunterMahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-04-06 10:35:33 +0600

    Create Linear Algebra Foundations #3- Matrix Multiplication.cpp

---
+++ Linear Algebra Foundations #3- Matrix Multiplication.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    vector<int> ar={18,21,29,34};
+    for(int i: ar){
+        cout<<i<<endl;
+    }
+    return 0;
+}
